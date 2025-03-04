9b5b6e452ff804b61c689d19518c1cf8cf847427
Lim <linus.lim@ubc.ca>
2025-03-04 14:06:57 -0800

    First stage Prototype

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   password: { type: String, required: true },
-  shifts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Shift' }]
+  shifts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Shift' }],
+  totalHours: { type: Number, default: 0 },
+  isCheckedIn: { type: Boolean, default: false } // Add this field
 });
 
 module.exports = mongoose.model('User', UserSchema);
